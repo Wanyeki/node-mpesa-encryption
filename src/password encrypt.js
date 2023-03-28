bc706bce22fa413aec633cb3dca26002b4746cf1
Francis Wanyeki <33685247+Wanyeki@users.noreply.github.com>
2023-03-28 16:52:10 +0300

    array buffer view

--- src/password encrypt.js
+++ src/password encrypt.js
@@ -3,22 +3,25 @@ const path = require('path');
 const crypto = require('crypto'); // comes with node
 
 
-exports.encryptPassword=(initiatorPassword)=>{
+exports.encryptPassword = (initiatorPassword) => {
     //read your cert to a buffer
-    const certFile = fs.readFileSync(path.join(__dirname, '../my_certificate.cer'));
+    const certFile = fs.readFileSync(path.join(__dirname, '../ProductionCertificate.cer'));
 
     //create X509Certificate from the buffer
     const cert = new crypto.X509Certificate(certFile);
 
     //extract public key from X509Certificate
-    const publicKey=cert.publicKey;
+    const publicKey = cert.publicKey
+    // console.log(publicKey.export({ format: 'pem', type: 'spki' }))
 
-    // get public key string 
-    console.log(publicKey.export({ format: 'pem', type: 'spki' }))
+  
+    //create arrayBufferView from the password
+    const encoder = new TextEncoder();
+    const arrayBuffer = encoder.encode(initiatorPassword).buffer;
+    const arrayBufferView = new Uint8Array(arrayBuffer);
 
-
-    // encrypt your password
-    securityCredential = crypto.publicEncrypt(publicKey,Buffer.from(initiatorPassword)).toString('base64');
+  // encrypt your password
+    securityCredential = crypto.publicEncrypt(publicKey, arrayBufferView).toString('base64');
     console.log(securityCredential);
 
     return securityCredential;
